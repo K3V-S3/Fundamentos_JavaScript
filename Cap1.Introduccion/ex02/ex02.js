3bce1637c922993de934dbe089791d3783820e18
Kevin <kevinse24@gmail.com>
2026-05-28 17:00:32 -0500

    Actualizado

---
+++ Cap1.Introduccion/ex02/ex02.js
@@ -0,0 +1,33 @@
+var nombre; // Declaración de la variable 'nombre'
+nombre = "Juan"; // Asignación de un valor a la variable 'nombre'
+console.log(nombre); // Muestra el valor de la variable 'nombre' en la consola
+
+var nombre = "María"; // Declaración y asignación de la variable 'nombre' en una sola línea
+console.log(nombre); // Muestra el nuevo valor de la variable 'nombre' en la consola
+var nombre = "Carlos"; // Declaración y asignación de la variable 'nombre' en una sola línea
+console.log(nombre); // Muestra el nuevo valor de la variable 'nombre' en la consola
+var nambre = "Ana"; // Declaración y asignación de la variable 'nombre' en una sola línea
+console.log(nambre); // Muestra el valor de la variable 'nambre' en la consola
+
+
+
+
+
+/*
+    Uso del la palabra reservada 'let' para declarar una variable llamada 'bebida' y asignarle diferentes valores a lo largo del código. Cada vez que se asigna un nuevo valor a la variable 'bebida', se muestra su contenido en la consola.
+
+    Diferencia entre var y let: La palabra reservada 'var' tiene un alcance de función, lo que significa que la variable declarada con 'var' es accesible dentro de la función en la que se declara. Por otro lado, 'let' tiene un alcance de bloque, lo que significa que la variable declarada con 'let' solo es accesible dentro del bloque en el que se declara (por ejemplo, dentro de un bucle o una estructura condicional). Además, 'let' no permite redeclarar la misma variable dentro del mismo bloque, mientras que 'var' sí lo permite.
+*/
+
+let bebida; // Declaración de la variable 'bebida'
+bebida = "Coca-Cola"; // Asignación de un valor a la variable 'bebida'
+
+console.log(bebida); // Muestra el valor de la variable 'bebida' en la consola
+let bebida ="Fanta"; // Declaración y asignación de la variable 'bebida'
+
+console.log(bebida); // Muestra el nuevo valor de la variable 'bebida' en la consola
+
+let bebida = "Sprite"; // Declaración y asignación de la variable 'bebida'
+console.log(bebida); // Muestra el nuevo valor de la variable 'bebida' en la consola
+
+// Uso de const para declarar una variable constante llamada 'PI' y asignarle el valor de 3.14159. Luego, se muestra el valor de 'PI' en la consola. Intentar reasignar un nuevo valor a 'PI' resultará en un error, ya que las variables declaradas con 'const' no pueden ser reasignadas después de su inicialización.
